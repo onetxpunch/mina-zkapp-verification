c1f217f93f72b73286b5997f4a53f78ba33cc823
punch <117326984+onetxpunch@users.noreply.github.com>
2023-11-04 08:24:40 -0400

    zkapp fetching callback

--- index.ts
+++ index.ts
@@ -1,1 +1,50 @@
-console.log("Hello via Bun!");
+import {
+  Field,
+  PrivateKey,
+  PublicKey,
+  SmartContract,
+  State,
+  method,
+  state,
+  Mina,
+  AccountUpdate,
+} from "o1js";
+import amqp from "amqplib/callback_api";
+
+let Local = Mina.LocalBlockchain({ proofsEnabled: false });
+Mina.setActiveInstance(Local);
+const feePayer1 = Local.testAccounts[0];
+
+amqp.connect(process.env.BROKER, (err, conn) => {
+  if (err) {
+    throw err;
+  }
+  conn.createChannel(async (e, chan) => {
+    if (e) {
+      throw e;
+    }
+
+    const index = "1";
+    chan.assertQueue(index, {
+      durable: true,
+    });
+
+    chan.consume(index, async (msg) => {
+      if (msg !== null) {
+        const data = JSON.parse(`${msg.content}`);
+
+        const { zkAppPublicKey, expectedKey, inputs } = data;
+
+        const appData = Mina.getAccount(zkAppPublicKey).zkapp;
+
+        let RuntimeInstanceResult: any; //
+
+        let deployedAppInstance = new RuntimeInstanceResult();
+
+        // !
+
+        chan.ack(msg);
+      }
+    });
+  });
+});
